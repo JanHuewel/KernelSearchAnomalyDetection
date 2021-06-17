6c17e62169613f08a363cf173ff4660c4fdf5151
JanHuewel <47003263+JanHuewel@users.noreply.github.com>
2021-06-17 14:45:47 +0200

    new data: dd_test_two_patterns0.csv

--- data/createData.py
+++ data/createData.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 data = dict()
 
 def f0(x):
-    return -3.0
+    return np.sin(0.1 * x)
 
 def f1(x):
-    return (0.1 * x) #+ 3*np.sin(x/3)
+    return 0.01 * x - 4
 
 def f2(x):
     return (0.01 * x - 10)
@@ -23,17 +23,16 @@ def f5(x):
     return 0.5 #* np.sin(x/5)
 
 def f(x):
-    return (x < 400 or x > 500) * f0(x) \
-           + (400<=x < 450) * f3(x) \
-           + (450<=x <=500) * f4(x)
+    return (x <= 400) * f0(x) \
+           + (x > 400) * f1(x)
 
 data["X"] = np.linspace(0, 1000, 1000)
-data["sigma"] = 0.004
+data["sigma"] = 0.1
 data["Y"] = np.vectorize(f)(data["X"]) + np.random.normal(0.0, data["sigma"], len(data["X"]))
 
 df = pd.DataFrame(data)
 
-df.to_csv('dd_test_basic_anomaly4.csv')
+df.to_csv('dd_test_2_patterns0.csv')
 
 plt.scatter(x = data["X"], y = data["Y"], marker = ".", linewidth = 0.001)
 plt.show()
