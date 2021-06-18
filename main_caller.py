4c208a8bbaad6f47ef552f7e89c96d289261ae55
Andreas Besginow <abesginow@gmail.com>
2021-06-18 10:54:54 +0200

    added test_config in main_caller for MWE test

--- main_caller.py
+++ main_caller.py
@@ -2,16 +2,15 @@ import subprocess
 import numpy as np
 list_of_data = ["data/dd_test_basic_anomaly2.csv"]
 list_of_segment_lengths = ["100"]
-list_of_methods = ["cov"] #"likelihood", "MSE", "KLD", "sampling", "sampling2"]
+list_of_methods = ["cov", "likelihood", "MSE", "KLD", "sampling", "sampling2"]
 list_of_clusterings = ["PIC", "Agg"]
 list_of_normalizations = ["0",'1','2']
 list_of_ground_truths = [["0","0","0","0","1","0","0","0","0","0"]]
 
-data_gt_pairs = zip(list_of_data, list_of_ground_truths)
+config = product(list_of_data, list_of_segment_lengths, list_of_methods, list_of_clusterings, list_of_normalizations)
 
-config = product(data_gt_pairs[0], list_of_segment_lengths, list_of_methods, list_of_clusterings, list_of_normalizations, data_gt_pairs[1])
-
-cmd = ['python', 'Main.py', config]
+test_config = ["data/dd_test_basic_anomaly2.csv", "100", "cov", "PIC", "0"]
+cmd = ['python', 'Main.py'] + test_config
 # todo, insert the new cfg file and the folder for the .data file
 #popen = subprocess.Popen(, stdout=subprocess.PIPE, universal_newlines=True)
 def execute(cmd):
@@ -25,7 +24,7 @@ def execute(cmd):
 
 # Example
 for conf in config:
-    for path in execute(conf):
+    for path in execute(cmd + conf):
     print(path, end="")
     pass
 
