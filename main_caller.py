192dc34b5c2e6cb7d62e798d9e26cb096c13df39
Andreas Besginow <abesginow@gmail.com>
2021-07-01 17:08:09 +0200

    deletes whatever is inside 'Results/' after experiment is zipped

--- main_caller.py
+++ main_caller.py
@@ -42,3 +42,16 @@ for i in range(10):
         pass
     folder = "Results/"
     shutil.make_archive(f"trial_{i}", 'zip', os.path.join(os.getcwd(), folder))
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+
+
